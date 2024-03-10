5115f28eb7dc2df54a22d864f934e4d5e9325348
Thal Adikary <thaladikary@gmail.com>
2024-03-10 12:43:50 -0600

    first commit

---
+++ public/movie-data.js
@@ -0,0 +1,48 @@
+
+const movieData = [
+    {   id: 2, title: "The Princess Diaries", tagline: "It can take a lifetime to find true love; she's got 30 days!",
+        poster: "/w9Z7A0GHEhIp7etpj0vyKOeU1Wx.jpg", runtime: 113, tmdbID: 11130
+    },     
+    {   id: 11, title: "Rocky", tagline: "His whole life was a million-to-one shot",
+        poster: "/i5xiwdSsrecBvO7mIfAJixeEDSg.jpg", runtime: 119, tmdbID: 1366
+    },  
+    {   id: 13, title: "American Beauty", tagline: "Look Closer",
+        poster: "/or1MP8BZIAjqWYxPdPX724ydKar.jpg", runtime: 122, tmdbID: 14
+    },  
+    {   id: 14, title: "Be Cool", tagline: "Everyone is looking for the next big hit",
+        poster: "/ekKCH7Zkb5ZTr1f1AczZY23FH.jpg", runtime: 118, tmdbID: 4551
+    },  
+    {   id: 15, title: "Minority Report", tagline: "The system is perfect until it comes after you",
+        poster: "/h3lpltSn7Rj1eYTPQO1lYGdw4Bz.jpg", runtime: 145, tmdbID: 180
+    },  
+    {   id: 21, title: "Lost in Space", tagline: "Danger, Will Robinson!",
+        poster: "/kkBDPkUnYwAkWbYY2bnyf5ANObA.jpg", runtime: 130, tmdbID: 2157
+    },  
+    {   id: 30, title: "Changing Lanes", tagline: "An ambitious lawyer, a desperate father, they had no reason to meet, until today",
+        poster: "/dCTVH1kl3aIkUmWpMT5STLAwsPs.jpg", runtime: 89, tmdbID: 1537
+    },  
+    {   id: 65, title: "Nebraska", tagline: "Life's not about winning or losing. It's about how you get there in the end.",
+        poster: "/zyG3vYWK7SwWT4bvrWeFef0J7fp.jpg", runtime: 115, tmdbID: 129670
+    },  
+    {   id: 71, title: "The Jacket", tagline: "Terror has a new name",
+        poster: "/ts5hSSpgHEcPFpdfk6gmLLgx5k4.jpg", runtime: 103, tmdbID: 9667
+    },  
+    {   id: 95, title: "Mulholland Drive", tagline: "An actress longing to be a star. A woman searching for herself. Both worlds will collide... on Mulholland Drive",
+        poster: "/oKyY4TFaLjQTgyX8oRde82GinOw.jpg", runtime: 147, tmdbID: 1018
+    },   
+    {   id: 102, title: "A Beautiful Mind", tagline: "I need to believe that something extra ordinary is possible",
+        poster: "/4SFqHDZ1NvWdysucWbgnYlobdxC.jpg", runtime: 135, tmdbID: 453
+    },
+    {   id: 128, title: "The Gift", tagline: "The only witness to the crime was not even there",
+        poster: "/mC66QbJk1PbUrVMu3kh14adLpQl.jpg", runtime: 111, tmdbID: 328425
+    },
+    {   id: 180, title: "Licence to Kill", tagline: "His bad side is a dangerous place to be",
+        poster: "/27EySoBH3oBuI6xat2QOquB9X8j.jpg", runtime: 133, tmdbID: 709
+    },
+    {   id: 212, title: "Jaws", tagline: "Don't go in the water",
+        poster: "/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg", runtime: 124, tmdbID: 578
+    },                                                     
+    {   id: 229, title: "The Wild Bunch", tagline: "If you only want to spend two hours in a movie theatre and go home and forget it, stay away from THE WILD BUNCH",
+        poster: "/zZhp7p8qvfVrSLKpOFHcKjpEj8f.jpg", runtime: 145, tmdbID: 576
+    }
+];
